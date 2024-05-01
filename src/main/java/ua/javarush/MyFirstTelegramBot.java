c5a14a4abeeed9cdf7c0b5e8d10b84b9fd4bf927
Andrii Ferenchuk <ferenchuk.a@hotmail.com>
2024-05-02 01:15:35 +0300

    fixing

--- src/main/java/ua/javarush/MyFirstTelegramBot.java
+++ src/main/java/ua/javarush/MyFirstTelegramBot.java
@@ -30,48 +30,44 @@ public class MyFirstTelegramBot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
         Long chatId = getChatId(update);
         if (update.hasMessage() && update.getMessage().getText().equals("/start")) {
-            addGlories(chatId, 0);
-            SendMessage message = createMessage(chatId, STEP_1_TEXT, Map.of("Злам холодильника", "step_1_btn"));
-            sendApiMethodAsync(message);
-        };
+            clearGlories(chatId);
+            sendMessage(chatId, 0, "step_1_pic", STEP_1_TEXT, Map.of("Злам холодильника", "step_1_btn"));
+        }
         if (update.hasCallbackQuery() && getGlories(chatId) == 0){
             if (update.getCallbackQuery().getData().equals("step_1_btn")) {
-                addGlories(chatId, 20);
-                SendMessage message = createMessage(chatId, STEP_2_TEXT, Map.of("Взяти сосиску! +20 слави", "step_2_btn", "Взяти рибку! +20 слави","step_2_btn", "Скинути банку з огірками! +20 слави", "step_2_btn"));
-                sendApiMethodAsync(message);
+                sendMessage(chatId, 20, "step_2_pic", STEP_2_TEXT, Map.of("Взяти сосиску! +20 слави", "step_2_btn", "Взяти рибку! +20 слави","step_2_btn", "Скинути банку з огірками! +20 слави", "step_2_btn"));
             }
         }
         if (update.getCallbackQuery().getData().equals("step_2_btn") && getGlories(chatId) == 20) {
-            addGlories(chatId, 20);
-            SendMessage message = createMessage(chatId, STEP_3_TEXT, Map.of("Злам робота пилососа", "step_3_btn"));
-            sendApiMethodAsync(message);
+            sendMessage(chatId, 20, "step_3_pic", STEP_3_TEXT, Map.of("Злам робота пилососа", "step_3_btn"));
         }
         if (update.getCallbackQuery().getData().equals("step_3_btn") && getGlories(chatId) == 40) {
-            addGlories(chatId, 30);
-            SendMessage message = createMessage(chatId, STEP_4_TEXT, Map.of("Відправити робопилосос за їжею! +30 слави!", "step_4_btn", "Проїхатися на робопилососі! +30 слави", "step_4_btn", "Тікати від робопилососа! +30 слави", "step_4_btn"));
-            sendApiMethodAsync(message);
+            sendMessage(chatId, 30, "step_4_pic", STEP_4_TEXT, Map.of("Відправити робопилосос за їжею! +30 слави!", "step_4_btn", "Проїхатися на робопилососі! +30 слави", "step_4_btn", "Тікати від робопилососа! +30 слави", "step_4_btn"));
         }
         if (update.getCallbackQuery().getData().equals("step_4_btn") && getGlories(chatId) == 70) {
-            addGlories(chatId, 30);
-            SendMessage message = createMessage(chatId, STEP_5_TEXT, Map.of("Одягнути та включити GoPro!", "step_5_btn"));
-            sendApiMethodAsync(message);
+            sendMessage(chatId, 30, "step_5_pic", STEP_5_TEXT, Map.of("Одягнути та включити GoPro!", "step_5_btn"));
         }
         if (update.getCallbackQuery().getData().equals("step_5_btn") && getGlories(chatId) == 100) {
-            addGlories(chatId, 30);
-            SendMessage message = createMessage(chatId, STEP_6_TEXT, Map.of("Бігати дахами, знімати на GoPro! +40 слави", "step_6_btn", "З GoPro нападати на інших котів із засідки! +40 слави", "step_6_btn", "З GoPro нападати на собак із засідки! +40 слави", "step_6_btn"));
-            sendApiMethodAsync(message);
+            sendMessage(chatId, 40, "step_6_pic", STEP_6_TEXT, Map.of("Бігати дахами, знімати на GoPro! +40 слави", "step_6_btn", "З GoPro нападати на інших котів із засідки! +40 слави", "step_6_btn", "З GoPro нападати на собак із засідки! +40 слави", "step_6_btn"));
         }
-        if (update.getCallbackQuery().getData().equals("step_6_btn") && getGlories(chatId) == 130) {
-            addGlories(chatId, 40);
-            SendMessage message = createMessage(chatId, STEP_7_TEXT, Map.of("Злам пароля", "step_7_btn"));
-            sendApiMethodAsync(message);
+        if (update.getCallbackQuery().getData().equals("step_6_btn") && getGlories(chatId) == 140) {
+            sendMessage(chatId, 40, "step_7_pic", STEP_5_TEXT, Map.of("Злам пароля", "step_7_btn"));
         }
-        if (update.getCallbackQuery().getData().equals("step_7_btn") && getGlories(chatId) == 170) {
-            SendMessage message = createMessage(chatId, STEP_8_TEXT, Map.of("Вийти на подвір'я", "step_8_btn"));
-            sendApiMethodAsync(message);
+        if (update.getCallbackQuery().getData().equals("step_7_btn") && getGlories(chatId) == 180) {
+            sendMessage(chatId, 50, "step_8_pic", STEP_8_TEXT, Map.of("Вийти на подвір'я", "step_8_btn"));
+        }
+        if (update.getCallbackQuery().getData().equals("step_8_btn") && getGlories(chatId) == 230) {
+            sendMessage(chatId,50,"final_pic", FINAL_TEXT, null);
         }
     }
 
+    private void sendMessage(Long chatId, int glories, String picName, String text, Map<String, String> buttons) {
+            addGlories(chatId, glories);
+            SendPhoto photoMessage = createPhotoMessage(chatId, picName);
+            executeAsync(photoMessage);
+            SendMessage message = createMessage(chatId, text, buttons);
+            sendApiMethodAsync(message);
+    }
     public static void main(String[] args) throws TelegramApiException {
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
         telegramBotsApi.registerBot(new MyFirstTelegramBot());
